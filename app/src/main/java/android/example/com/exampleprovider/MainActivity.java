33c40fa6590c6a1f504530ec3799d7916636a1f1
Lyla <lyla@udacity.com>
2014-11-07 11:00:36 -0800

    Remove menu

--- app/src/main/java/android/example/com/exampleprovider/MainActivity.java
+++ app/src/main/java/android/example/com/exampleprovider/MainActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package android.example.com.exampleprovider;
 
 import android.content.ContentResolver;
@@ -6,17 +21,14 @@ import android.database.Cursor;
 import android.example.com.exampleprovider.data.ExampleContract.ExampleEntry;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 
 public class MainActivity extends ActionBarActivity {
-
-
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
     ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +37,7 @@ public class MainActivity extends ActionBarActivity {
 
         insertData();
 
-        Cursor cursor = this.getContentResolver().query(ExampleEntry.TABLE_URI,null, null,null,null);
+        Cursor cursor = this.getContentResolver().query(ExampleEntry.CONTENT_URI,null, null,null,null);
         mListView = (ListView) findViewById(R.id.main_list_view);
         ContentResolver resolver = getContentResolver();
         String[] columnsToBeBound = new String[] {
@@ -51,30 +63,10 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     private void insertData() {
         ContentResolver resolver =  this.getContentResolver();
 
-        Cursor cursor = resolver.query(ExampleEntry.TABLE_URI, null,null,null,null);
+        Cursor cursor = resolver.query(ExampleEntry.CONTENT_URI, null,null,null,null);
         if (cursor.getCount() == 0) {
 
             String nameOne = "Dan";
@@ -92,7 +84,7 @@ public class MainActivity extends ActionBarActivity {
             values[1].put(ExampleEntry.NAME, nameTwo);
             values[1].put(ExampleEntry.NUMBER_OF_FRIENDS, friendsTwo);
 
-            resolver.bulkInsert(ExampleEntry.TABLE_URI,values);
+            resolver.bulkInsert(ExampleEntry.CONTENT_URI,values);
         }
         cursor.close();
     }
